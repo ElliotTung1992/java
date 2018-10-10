f88224c71ee8c4719803b401993deb11e4c4fa65
dge1992 <dge_199@163.com>
2018-10-10 23:09:28 +0800

    面向对象之多态经典案例

---
+++ src/main/java/com/github/demo/java/characteristic/Test.java
@@ -0,0 +1,58 @@
+package com.github.demo.java.characteristic;
+
+/**
+ * Java的4大特性：封装 继承 抽象 多态
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        A a1 = new A();
+        A a2 = new B();
+        B b = new B();
+        C c = new C();
+        D d = new D();
+
+        System.out.println("1 -> " + a1.show(b));
+        System.out.println("2 -> " + a1.show(c));
+        System.out.println("3 -> " + a1.show(d));
+        System.out.println("4 -> " + a2.show(b));
+        System.out.println("5 -> " + a2.show(c));
+        System.out.println("6 -> " + a2.show(d));
+        System.out.println("7 -> " + b.show(b));
+        System.out.println("8 -> " + b.show(c));
+        System.out.println("9 -> " + b.show(d));
+    }
+}
+
+//经典多态案例
+class A {
+
+    public String show(D obj){
+        return "A and D";
+    }
+
+    public String show(A obj){
+        return "A and A";
+    }
+}
+
+class B extends A {
+
+    public String show(B obj){
+        return "B and B";
+    }
+
+    public String show(A obj){
+        return "B and A";
+    }
+
+}
+
+class C extends B {
+
+}
+
+class D extends B {
+
+}
+
