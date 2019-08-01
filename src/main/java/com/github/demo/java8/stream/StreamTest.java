e295f51d772e79c950cf6109be28c4998273424e
dge1992 <dge_1992@163.com>
2019-08-01 15:32:26 +0800

    数据流

--- src/main/java/com/github/demo/java8/stream/StreamTest.java
+++ src/main/java/com/github/demo/java8/stream/StreamTest.java
@@ -5,6 +5,7 @@ import com.github.demo.domain.Foo;
 import com.github.demo.domain.User;
 
 import java.util.*;
+import java.util.concurrent.ForkJoinPool;
 import java.util.function.Supplier;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
@@ -50,7 +51,48 @@ public class StreamTest {
         //test5();
         //test6();
         //test7();
-        test8();
+        //test8();
+        test9();
+    }
+
+    private static void test9() {
+        ForkJoinPool commonPool = ForkJoinPool.commonPool();
+        System.out.println(commonPool.getParallelism());
+
+        Arrays.asList("a1", "a2", "b1", "c2", "c1")
+                .parallelStream()
+                .filter(s -> {
+                    System.out.format("filter: %s [%s]\n",
+                            s, Thread.currentThread().getName());
+                    return true;
+                })
+                .map(s -> {
+                    System.out.format("map: %s [%s]\n",
+                            s, Thread.currentThread().getName());
+                    return s.toUpperCase();
+                })
+                .forEach(s -> System.out.format("forEach: %s [%s]\n",
+                        s, Thread.currentThread().getName()));
+
+        Arrays.asList("a1", "a2", "b1", "c2", "c1")
+                .parallelStream()
+                .filter(s -> {
+                    System.out.format("filter: %s [%s]\n",
+                            s, Thread.currentThread().getName());
+                    return true;
+                })
+                .map(s -> {
+                    System.out.format("map: %s [%s]\n",
+                            s, Thread.currentThread().getName());
+                    return s.toUpperCase();
+                })
+                .sorted((s1, s2) -> {
+                    System.out.format("sort: %s <> %s [%s]\n",
+                            s1, s2, Thread.currentThread().getName());
+                    return s1.compareTo(s2);
+                })
+                .forEach(s -> System.out.format("forEach: %s [%s]\n",
+                        s, Thread.currentThread().getName()));
     }
 
     private static void test8() {
@@ -62,9 +104,29 @@ public class StreamTest {
         users.add(new User(5, "name" + 5, 55));
         users.add(new User(6, "name" + 6, 55));
 
-        users.stream()
-                .reduce((p1, p2) -> p1.getAge() > p2.getAge() ? p1 : p2)
-                .ifPresent(System.out::println);
+//        users.stream()
+//                .reduce((p1, p2) -> p1.getAge() > p2.getAge() ? p1 : p2)
+//                .ifPresent(System.out::println);
+//
+//        Integer ageSum = users
+//                .stream()
+//                .reduce(0, (sum, p) -> sum += p.getAge(), (sum1, sum2) -> sum1 + sum2);
+//
+//        System.out.println(ageSum);
+
+        Integer ageSum = users
+                .parallelStream()
+                .reduce(0,
+                        (sum, p) -> {
+                            System.out.format("accumulator: sum=%s; person=%s\n", sum, p);
+                            return sum += p.getAge();
+                        },
+                        (sum1, sum2) -> {
+                            System.out.format("combiner: sum1=%s; sum2=%s\n", sum1, sum2);
+                            return sum1 + sum2;
+                        });
+
+        System.out.println(ageSum);
     }
 
     private static void test7() {
